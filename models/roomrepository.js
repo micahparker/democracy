decc21c93041d794eea5a36edcb5cb66de95c0b9
Micah Parker <micahparker@gmail.com>
2015-11-18 14:06:04 -0700

    little more organized

---
+++ models/roomrepository.js
@@ -0,0 +1,54 @@
+module.exports = function (_DB) {
+    var uuid = require('node-uuid');
+    
+    return {
+        createRoom: function (name) {
+            return {
+                name: name || uuid.v4().replace(new RegExp("-", "g"), ""),
+                description: null,
+                users: [],
+                lastaccessed: new Date()
+            };
+        },
+
+        addRoom: function (room) {
+            _DB.insert(room);
+            //remove an old, unused one
+            this.deleteOldRoom();
+            //return attached
+            return room;
+        },
+    
+        updateRoom: function (room) {
+            _DB.update(room);
+            return null;
+        },
+    
+        findRoom: function (name) {
+            var rooms = _DB.find({ "name": name });
+            for (var x=0; x < rooms.length; x++) {
+                return rooms[x];
+            }
+            return null;
+        },
+    
+        deleteOldRoom: function () {
+            var now = new Date();
+            for (var room in _DB.data) {
+                if ((now - room.lastaccessed) >= 3600000) {
+                    _DB.remove(room);
+                    break;
+                }
+            }
+        },
+    
+        findUser: function (room, id) {
+            for (var x = 0; x < room.users.length; x++) {
+                if (room.users[x].id === id) {
+                    return room.users[x];
+                }
+            }
+            return null;
+        }
+    }
+};
--- routes/rooms.js
+++ routes/rooms.js
@@ -1,7 +1,7 @@
 module.exports = function (_IO, _DB) {
     var express = require('express');
     var router = express.Router();
-    var uuid = require('node-uuid');
+    var repo = require('../models/roomrepository.js')(_DB);
     
     var ok = function (res) {
         res.status(200).send(null).end();
@@ -15,80 +15,32 @@ module.exports = function (_IO, _DB) {
         res.status(404).send("Not Found").end();
     }
     
-    var _createRoom = function (name) {
-        return {
-            name: name || uuid.v4().replace(new RegExp("-", "g"), ""),
-            description: null,
-            users: [],
-            lastaccessed: new Date()
-        };
-    }
-
-    var _addRoom = function (room) {
-        _DB.insert(room);
+    var _initIO = function (room) {
         //create socket for the room
         _IO.of("/" + room.name).on('connection', function (socket) {
             socket.emit("room", JSON.stringify(room));
         });
-        //remove an old, unused one
-        _deleteOldRoom();
-        //return attached
-        return room;
-    }
-    
-    var _updateRoom = function (room) {
-        _DB.update(room);
-        return null;
-    }
-    
-    var _findRoom = function (name) {
-        var rooms = _DB.find({ "name": name });
-        for (var x=0; x < rooms.length; x++) {
-            return rooms[x];
-        }
-        return null;
-    }
-    
-    var _deleteOldRoom = function () {
-        var now = new Date();
-        for (var room in _DB.data) {
-            if (now - room.lastaccessed >= 3600000) {
-                _DB.remove(room);
-                break;
-            }
-        }
-    }
-    
-    var _findUser = function (room, id) {
-        for (var x = 0; x < room.users.length; x++) {
-            if (room.users[x].id === id) {
-                return room.users[x];
-            }
-        }
-        return null;
-    }
-    
-    var _isRoomMatch = function (client, room) {
-        return true;
     }
     
     //routes
     
     router.get('/:name', function (req, res, next) {
-        var room = _findRoom(req.params.name);
+        var room = repo.findRoom(req.params.name);
         if (room) {
             return json(res, room);
         }
         else {
-            _addRoom(_createRoom(req.params.name))
+            room = repo.createRoom(req.params.name);
+            repo.addRoom(room);
+            _initIO(room);
         }
         return notfound(res);
     });
     
     router.get('/:name/user', function (req, res, next) {
-        var room = _findRoom(req.params.name);
+        var room = repo.findRoom(req.params.name);
         if (room) {
-            var user = _findUser(room, req.session.id);
+            var user = repo.findUser(room, req.session.id);
             if (user) {
                 return json(res, user);
             }
@@ -97,7 +49,7 @@ module.exports = function (_IO, _DB) {
     });
     
     router.put('/:name', function (req, res, next) {
-        var room = _findRoom(req.params.name);
+        var room = repo.findRoom(req.params.name);
         if (room) {
             if (req.body.description) {
                 room.description = req.body.description;
@@ -121,9 +73,9 @@ module.exports = function (_IO, _DB) {
     });
     
     router.put('/:name/user', function (req, res, next) {
-        var room = _findRoom(req.params.name);
+        var room = repo.findRoom(req.params.name);
         if (room) {
-            var user = _findUser(room, req.session.id);
+            var user = repo.findUser(room, req.session.id);
             if (user) {
                 if (req.body.name) {
                     user.name = req.body.name;
@@ -148,17 +100,19 @@ module.exports = function (_IO, _DB) {
     });
     
     router.post('/', function (req, res, next) {
-        var room = _createRoom();
+        var room = repo.createRoom();
         //add to db
-        _addRoom(room);
+        repo.addRoom(room);
+        //initialize a socket namespace
+        _initIO(room);
         //return
         return json(res, room);
     });
     
     router.post('/:name/user', function (req, res, next) {
-        var room = _findRoom(req.params.name);
+        var room = repo.findRoom(req.params.name);
         if (room) {
-            if (!_findUser(room, req.session.id)) {
+            if (!repo.findUser(room, req.session.id)) {
                 var user = {
                     id: req.session.id,
                     name: req.body.name,
@@ -186,9 +140,9 @@ module.exports = function (_IO, _DB) {
     });
     
     router.post('/:name/vote/:vote', function (req, res, next) {
-        var room = _findRoom(req.params.name);
+        var room = repo.findRoom(req.params.name);
         if (room) {
-            var user = _findUser(room, req.session.id);
+            var user = repo.findUser(room, req.session.id);
             if (user) {
                 var vote = parseInt(req.params.vote);
                 if (vote) {
@@ -211,9 +165,9 @@ module.exports = function (_IO, _DB) {
     });
     
     router.delete('/:name/user/:id', function (req, res, next) {
-        var room = _findRoom(req.params.name);
+        var room = repo.findRoom(req.params.name);
         if (room) {
-            var user = _findUser(room, req.params.id);
+            var user = repo.findUser(room, req.params.id);
             if (user) {
                 user.active = false;
                 //post to socket
